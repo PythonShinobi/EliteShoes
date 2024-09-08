9557e3c153ae04f5602aa75f11e926882e7c06be
PythonShinobi <pythonshinobi@gmail.com>
2024-09-08 14:14:13 +0300

    Add route path for SneakersPage, HighHeelsPage, BootsPage and SportShoesPage.

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,24 @@
 import React, { memo } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Home from "./home/Home"
+import Home from "./home/Home";
 import ContactPage from './contact/Contact';
 import Navbar from './navbar/Navbar';
 import Footer from './footer/Footer';
+
+import SneakersPage from './data/SneakersPage'; // Import SneakersPage
+import HighHeelsPage from './data/HighHeelsPage'
+import BootsPage from './data/BootsPage';
+import SportShoesPage from './data/SportShoesPage';
 
 // Memoized components
 const MemoizedComponents = {
   Home: memo(Home),
   ContactPage: memo(ContactPage),
+  SneakersPage: memo(SneakersPage), // Memoize SneakersPage
+  HighHeelsPage: memo(HighHeelsPage),
+  BootsPage: memo(BootsPage),
+  SportShoesPage: memo(SportShoesPage)
 };
 
 const App = () => {
@@ -19,9 +28,13 @@ const App = () => {
         <Navbar />
         <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
           <Routes>
-            <Route path='/' element={<MemoizedComponents.Home />} />            
+            <Route path='/' element={<MemoizedComponents.Home />} />
             <Route path='/contact' element={<MemoizedComponents.ContactPage />} />
-          </Routes>          
+            <Route path='/products/sneakers' element={<MemoizedComponents.SneakersPage />} /> {/* Add route for products */}
+            <Route path='/products/boots' element={<MemoizedComponents.BootsPage />} /> {/* Add route for products */}
+            <Route path='/products/high-heels' element={<MemoizedComponents.HighHeelsPage />} /> {/* Add route for products */}
+            <Route path='/products/sport-shoes' element={<MemoizedComponents.SportShoesPage />} /> {/* Add route for products */}
+          </Routes>
         </div>
         <Footer />
     </BrowserRouter>
