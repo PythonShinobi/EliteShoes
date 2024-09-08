e6468e46f3d052921e2b06c692ed689a103a22a5
PythonShinobi <pythonshinobi@gmail.com>
2024-09-08 10:56:45 +0300

    Add contact page to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import React, { memo } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Home from "./home/Home"
+import ContactPage from './contact/Contact';
 import Navbar from './navbar/Navbar';
 import Footer from './footer/Footer';
 
 // Memoized components
 const MemoizedComponents = {
-  Home: memo(Home)
+  Home: memo(Home),
+  ContactPage: memo(ContactPage),
 };
 
 const App = () => {
@@ -18,6 +20,7 @@ const App = () => {
         <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
           <Routes>
             <Route path='/' element={<MemoizedComponents.Home />} />            
+            <Route path='/contact' element={<MemoizedComponents.ContactPage />} />
           </Routes>          
         </div>
         <Footer />
